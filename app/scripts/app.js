201e91a08c0e398fec665dd164299febbcaeb9ed
playing <deadplaying@gmail.com>
2014-03-21 16:31:02 +0800

    基本前端项目框架+部分主页代码 Signed-off-by: playing <deadplaying@gmail.com>

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('quickCastIndex', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        templateUrl: '404.html',
+      });
+  });
