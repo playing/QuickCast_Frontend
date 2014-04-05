6cb321da28a759082829997153683f9a79143233
playing <deadplaying@gmail.com>
2014-04-05 18:53:42 +0800

    user main page
    add user main page

--- app/scripts/common/common_service.js
+++ app/scripts/common/common_service.js
@@ -2,11 +2,38 @@
 angular.module('QuickCast')
 	.factory('register', function($http, $stateParams, $state) {
 		var register_instance = function(user_reg) {
-			var data1 = "test";
+			var register_data = {
+				"user_name": user_reg.user_name,
+				"password": user_reg.password,
+				"email": user_reg.email,
+				"cn_tname": user_reg.cn_tname,
+				"eng_name": user_reg.eng_name
+			};
 			$http({
-				method: 'get',
+				method: 'post',
 				url: 'http://114.215.168.150:8080/quickcast/user_reg.do?method=reg',
-				data: data1
+				data: register_data
+			}).
+			success(function(data, status) {
+				alert("success");
+				//$state.go('basicprofile');
+			}).
+			error(function(data, status) {
+				alert("failed");
+				//$state.go('profile');
+			});
+		}
+		return register_instance;
+	});
+
+angular.module('QuickCast')
+	.factory('profile', function($http, $stateParams, $state) {
+		var profile_instance = function(user_profile) {
+			var profile_data = {};
+			$http({
+				method: 'post',
+				url: 'user_reg.do?method=reg',
+				data: profile_data
 			}).
 			success(function(data, status) {
 				alert("success");
@@ -16,29 +43,7 @@ angular.module('QuickCast')
 				alert("failed");
 				$state.go('profile');
 			});
-		}
-		return register_instance;
-	});
 
-angular.module('QuickCast')
-	.factory('profile', function($http, $stateParams, $state) {
-		var profile_instance = function(user_profile) {
-			// 	var data1 = "test";
-			// 	$http({
-			// 		method: 'get',
-			// 		url: 'user_reg.do?method=reg',
-			// 		data: data1
-			// 	}).
-			// 	success(function(data, status) {
-			// 		alert("success");
-			// 		//$state.go('basicprofile');
-			// 	}).
-			// 	error(function(data, status) {
-			// 		alert("failed");
-			// 		$state.go('profile');
-			// 	});
-			// }
-			//工厂函数的函数体创建了NewServiceInstance
 			alert("profile");
 		}
 		return profile_instance;
@@ -47,19 +52,42 @@ angular.module('QuickCast')
 angular.module('QuickCast')
 	.factory('login', function($http, $stateParams, $state) {
 		var login_instance = function(user_login) {
-				var login_data = {"user_name":user_login.user_name,"password":user_login.password};
-				$http({
-					method: 'post',
-					url: 'http://114.215.168.150:8080/quickcast/user_reg.do?method=login',
-					data: login_data
-				}).
-				success(function(data, status) {
-					alert("success");
-					//$state.go('basicprofile');
-				}).
-				error(function(data, status) {
-					alert("failed");
-				});
-			}
+			var login_data = {
+				"user_name": user_login.user_name,
+				"password": user_login.password
+			};
+			$http({
+				method: 'post',
+				url: 'http://114.215.168.150:8080/quickcast/user_reg.do?method=login',
+				data: login_data
+			}).
+			success(function(data, status) {
+				alert("success");
+			}).
+			error(function(data, status) {
+				alert("failed");
+			});
+		}
 		return login_instance;
+	});
+angular.module('QuickCast')
+	.factory('checkname', function($http, $stateParams, $state) {
+		var checkname_instance = function(user_name) {
+			var checkname_data = user_name;
+			$http({
+				method: 'post',
+				url: 'user_reg.do?method=check_uname',
+				data: checkname_data
+			}).
+			success(function(data, status) {
+				alert("success");
+
+			}).
+			error(function(data, status) {
+				alert("failed");
+			});
+
+			alert("checkname");
+		}
+		return checkname_instance;
 	});
--- app/scripts/user/user_controller.js
+++ app/scripts/user/user_controller.js
@@ -2,26 +2,6 @@
 
 angular.module('QuickCastUser')
 	.controller('UserCtrl', function($scope,$location) {
-		alert("ss");
-		//$scope.$route = $route;
-		//$scope.$location = $location;
-		//$scope.$routeParams = $routeParams;
-		//var ss = $location.path();
-		//alert(ss);
-		//$scope.register = function() {
-		//	var ss = $location.path();
-		//	alert(ss);
-			//$location.path("/register");
-			//TO DO
-		//};
-		//$scope.login = function() {
-		//	var ss = $location.path();
-		//	alert(ss);
-			//TO DO
-		//};
-		//$scope.resetpassword = function() {
-		//	alert("ss");
-			//TO DO
-		//};
+
 
 	});
