0727eedb85b13ee73296e8b0a737b6e1428827c8
playing <deadplaying@gmail.com>
2014-05-16 15:43:07 +0800

    resume deliver
    resume deliver

--- app/scripts/search/search_controller.js
+++ app/scripts/search/search_controller.js
@@ -9,7 +9,13 @@ angular.module('QuickCastSearch')
 		$scope.searchicon = 'list';
 		$scope.active1 = 'active';
 		$scope.searchnum = 10;
-
+		$scope.langs = [];
+		$scope.projects = [];
+		$scope.edus = [];
+		$scope.works = [];
+		$scope.user_cookie = $cookieStore.get('_UDATA');
+		$scope.resumes = [];
+		$scope.alerts = [];
 		var init = function() {
 			SearchService.Etp().then(function(response) {
 				$scope.etps = response.etp;
@@ -17,7 +23,6 @@ angular.module('QuickCastSearch')
 			});
 			SearchService.Recruit().then(function(response) {
 				$scope.recruits = response.recruit_info;
-				console.log(response);
 			});
 
 			SearchService.Hunter().then(function(response) {
@@ -35,38 +40,168 @@ angular.module('QuickCastSearch')
 		};
 
 		$scope.back = function() {
-			var user_cookie = $cookieStore.get('_UDATA');
-			if (user_cookie.user_type === '1') {
-				$window.location.href = 'user.html#/user/' + user_cookie.user_id;
-
+			if ($scope.user_cookie.user_type === '1') {
+				$window.location.href = 'user.html#/user/' + $scope.user_cookie.user_id;
 			} else {
-				if (user_type === '2') {
-					$window.location.href = 'headhunter.html#/user/' + user_cookie.user_id;
+				if ($scope.user_cookie.user_type === '2') {
+					$window.location.href = 'headhunter.html#/user/' + $scope.user_cookie.user_id;
 				} else {
-					$window.location.href = 'company.html#/user/' + user_cookie.user_id;
+					$window.location.href = 'company.html#/user/' + $scope.user_cookie.user_id;
 				}
 			}
 		};
+
+		$scope.deliver = function(target_id, target_type, info_id) {
+			for (var i = 0; i < $scope.resumes.length; i++) {
+				if ($scope.resumes[i].$checked) {
+					var deliver_rsm = {};
+					if (target_type === '2') {
+						deliver_rsm = {
+							rsm_id: $scope.resumes[i].rsm_id,
+							hunter_id: target_id,
+							etp_id: 0,
+							info_id: info_id
+						};
+					} else {
+						deliver_rsm = {
+							rsm_id: $scope.resumes[i].rsm_id,
+							hunter_id: 0,
+							etp_id: target_id,
+							info_id: info_id
+						};
+					}
+					SearchService.DeliverRsm(deliver_rsm).then(function(response) {
+						if (response.result.data === 'success') {
+							$scope.alerts.push({
+								type: 'success',
+								msg: '投递成功.'
+							});
+						} else {
+							$scope.alerts.push({
+								type: 'danger',
+								msg: '请求失败,请尝试重新发送.'
+							});
+						}
+					});
+				}
+			}
+
+		};
+
+		$scope.tship = function(hunter_id) {
+			var tship_rsm = {
+				rsm_id: parseInt($scope.user_cookie.user_id),
+				hunter_id: hunter_id,
+				etp_id: 0,
+				info_id: 0
+			};
+			SearchService.DeliverRsm(tship_rsm).then(function(response) {
+				if (response.result.data === 'success') {
+					$scope.alerts.push({
+						type: 'success',
+						msg: '托管成功.'
+					});
+				} else {
+					$scope.alerts.push({
+						type: 'danger',
+						msg: '请求失败,请尝试重新发送.'
+					});
+				}
+			});
+		};
+
+		$scope.$on('$stateChangeStart',
+			function() {
+				$scope.alerts = [];
+				//视图切换时清空错误信息
+			});
 
 		$scope.$on('$stateChangeStart',
 			function() {
 				var location_array = $location.path().split('/');
 				$scope.index_id = location_array[2];
 				if (location_array[1] === 'job') {
-					console.log(location_array[1]);
 					var info_id = {
 						info_id: parseInt($scope.index_id)
 					};
 					SearchService.SingleRecruit(info_id).then(function(response) {
-						$scope.singlerecruit=response.recruit_info[0];
+						$scope.singlerecruit = response.recruit_info[0];
 					});
-					
+					if ($scope.user_cookie.user_type === '1') {
+						$scope.resumes = [{
+							rsm_id: $scope.user_cookie.user_id,
+							name: $scope.user_cookie.cn_tname
+						}];
+					} else {
+						SearchService.HunterRsm(parseInt($scope.user_cookie.user_id)).then(function(response) {
+							for (var i = 0; i < response.deliver.length; i++) {
+								$scope.resumes = [];
+								$scope.resumes.push({
+									rsm_id: response.deliver[i].rsm_id,
+									name: response.deliver[i].rsm_name
+								});
+							}
+						});
+					}
 				} else {
 					if (location_array[1] === 'member') {
-						console.log('member');
+						SearchService.SingleUser(parseInt($scope.index_id)).then(function(response) {
+							$scope.singleuser = response.user_info[0];
+							console.log(response);
+						});
+						SearchService.WorkExpGet(parseInt($scope.index_id)).then(function(response) {
+							$scope.works = response.work_exp;
+						});
+						SearchService.PrjExpGet(parseInt($scope.index_id)).then(function(response) {
+							$scope.projects = response.prj_exp;
+						});
+
+						SearchService.EduExpGet(parseInt($scope.index_id)).then(function(response) {
+							$scope.edus = response.edu_exp;
+						});
+
+						SearchService.ResumeGet(parseInt($scope.index_id)).then(function(response) {
+							$scope.langs = JSON.parse(response.resume[0].language_skill);
+						});
 
 					}
 				}
 				//视图切换时更新路径
 			});
+	})
+	.filter('usertype', function() {
+		return function(input) {
+			var out = '';
+			if (input === '1') {
+				out = '求职者';
+			} else {
+				if (input === '2') {
+					out = '猎头';
+					return out;
+				} else {
+					out = '公司';
+					return out;
+				}
+				out = '未确定';
+			}
+			return out;
+		};
+	})
+	.filter('usertypeeng', function() {
+		return function(input) {
+			var out = '';
+			if (input === '1') {
+				out = 'user';
+			} else {
+				if (input === '2') {
+					out = 'headhunter';
+					return out;
+				} else {
+					out = 'company';
+					return out;
+				}
+				out = '未确定';
+			}
+			return out;
+		};
 	});
--- app/scripts/search/search_service.js
+++ app/scripts/search/search_service.js
@@ -13,7 +13,7 @@ angular.module('QuickCastSearch')
 						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
 						return response_JSON;
 					});
-				return recruit_response
+				return recruit_response;
 			},
 			Hunter: function() {
 				var hunter_response = $http({
@@ -25,7 +25,7 @@ angular.module('QuickCastSearch')
 						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
 						return response_JSON;
 					});
-				return hunter_response
+				return hunter_response;
 			},
 			Etp: function() {
 				var etp_response = $http({
@@ -37,7 +37,7 @@ angular.module('QuickCastSearch')
 						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
 						return response_JSON;
 					});
-				return etp_response
+				return etp_response;
 			},
 			Seeker: function() {
 				var seeker_response = $http({
@@ -49,7 +49,7 @@ angular.module('QuickCastSearch')
 						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
 						return response_JSON;
 					});
-				return seeker_response
+				return seeker_response;
 			},
 			SingleRecruit: function(info_id) {
 				var singlerecruit_response = $http({
@@ -62,8 +62,112 @@ angular.module('QuickCastSearch')
 						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
 						return response_JSON;
 					});
-				return singlerecruit_response
+				return singlerecruit_response;
 			},
+
+			SingleUser: function(user_id) {
+				var singleuser_response = $http({
+						method: 'post',
+						url: Server + 'info.do?method=info_queryByUserId',
+						data: user_id
+					})
+					.then(function(response) {
+						var response_JSON = decodeURIComponent(decodeURIComponent(response.data));
+						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
+						return response_JSON;
+					});
+				return singleuser_response;
+			},
+			WorkExpGet: function(user_id) {
+				var workexp_get_data = {
+					'user_id': user_id
+				};
+				var workexp_get_response = $http({
+						method: 'post',
+						url: Server + 'exp.do?method=queryByWorkuserId',
+						data: workexp_get_data
+					})
+					.then(function(response) {
+						var response_JSON = decodeURIComponent(decodeURIComponent(response.data));
+						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
+						return response_JSON;
+					});
+				return workexp_get_response;
+			},
+			EduExpGet: function(user_id) {
+				var eduexp_get_data = {
+					'user_id': user_id
+				};
+				var eduexp_get_response = $http({
+						method: 'post',
+						url: Server + 'exp.do?method=queryByEduuserId',
+						data: eduexp_get_data
+					})
+					.then(function(response) {
+						var response_JSON = decodeURIComponent(decodeURIComponent(response.data));
+						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
+						return response_JSON;
+					});
+				return eduexp_get_response;
+			},
+			PrjExpGet: function(user_id) {
+				var prjexp_get_data = {
+					'user_id': user_id
+				};
+				var prjexp_get_response = $http({
+						method: 'post',
+						url: Server + 'exp.do?method=queryByPrjuserId',
+						data: prjexp_get_data
+					})
+					.then(function(response) {
+						var response_JSON = decodeURIComponent(decodeURIComponent(response.data));
+						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
+						return response_JSON;
+					});
+				return prjexp_get_response;
+			},
+			ResumeGet: function(user_id) {
+				var resume_get_data = {
+					'user_id': user_id
+				};
+				var resume_get_response = $http({
+						method: 'post',
+						url: Server + 'personal_rsm.do?method=imp_resume_queryByUserId',
+						data: resume_get_data
+					})
+					.then(function(response) {
+						var response_JSON = decodeURIComponent(decodeURIComponent(response.data));
+						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
+						return response_JSON;
+					});
+				return resume_get_response;
+			},
+			HunterRsm: function(hunter_id) {
+				var hunterrsm_response = $http({
+						method: 'post',
+						url: Server + 'rsm_deliver.do?method=imp_deliver_queryByHunterId',
+						data: hunter_id
+					})
+					.then(function(response) {
+						var response_JSON = decodeURIComponent(decodeURIComponent(response.data));
+						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
+						return response_JSON;
+					});
+				return hunterrsm_response;
+			},
+			DeliverRsm: function(deliver) {
+				var deliverrsm_response = $http({
+						method: 'post',
+						url: Server + 'rsm_deliver.do?method=imp_deliver_insert',
+						data: deliver
+					})
+					.then(function(response) {
+						var response_JSON = decodeURIComponent(decodeURIComponent(response.data));
+						response_JSON = JSON.parse(response_JSON.replace(/\+/g, ' '));
+						return response_JSON;
+					});
+				return deliverrsm_response;
+			}
 		};
 		return SearchService;
 	});
